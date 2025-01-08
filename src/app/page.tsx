8a006f6295e98e7a12a4edd73e59848e28f9931d
Hermes <hermeschigoziri@gmail.com>
2025-01-09 00:21:38 +0100

    make homepage responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
-import REPOImg from "../../public/REPO.png"
+import REPOImg from "../../public/REPO.png";
 
 export default function Home() {
 	return (
-		<div className="flex-grow flex items-center gap-8 bg-[#1A1A1A] p-20 px-40">
-			<div className="flex flex-col gap-2 max-w-2xl">
-				<h2 className="text-4xl font-bold">Manage Course Materials</h2>
-				<p className="text-3xl font-extralight">
+		<div className="flex-grow flex flex-col justify-center gap-8 bg-[#1A1A1A] p-4 xl:flex-row items-center">
+			<div className="flex flex-col gap-2 max-w-2xl px-8">
+				<h2 className="text-3xl font-bold">Manage Course Materials</h2>
+				<p className="text-2xl font-extralight">
 					REPO lets you compile various resources ranging from slides,
 					recommended texts, to online materials like videos and
 					webpages that explain some topic relevant to a course
 				</p>
 			</div>
-			<Image className="max-w-3xl h-max" src={REPOImg} alt="REPO features" />
+			<Image
+				className="w-auto xl:max-w-2xl 2xl:max-w-3xl h-max"
+				src={REPOImg}
+				alt="REPO's features"
+			/>
 		</div>
 	);
 }
