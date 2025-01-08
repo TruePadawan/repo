56f05232206750c393b66be60999827ccd8f79c8
Hermes <hermeschigoziri@gmail.com>
2025-01-08 17:46:25 +0100

    base files

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Albert_Sans } from "next/font/google";
+import "./globals.css";
+
+const albertSans = Albert_Sans({
+	variable: "--font-albert-sans",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "REPO",
+	description:
+		"REPO lets you compile various resources ranging from slides, recommended texts, to online materials like videos and webpages that explain some topic relevant to a course",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={`${albertSans.variable} antialiased`}>
+				{children}
+			</body>
+		</html>
+	);
+}
