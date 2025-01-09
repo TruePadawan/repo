32b8d9c5c5230be039f0010dc824a8a2f51bb489
Hermes <hermeschigoziri@gmail.com>
2025-01-09 11:42:39 +0100

    setup oauth via appwrite

---
+++ src/lib/server/oauth.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { createAdminClient } from "@/lib/server/appwrite";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { OAuthProvider } from "node-appwrite";
+
+export async function signUpWithGithub() {
+	const { account } = await createAdminClient();
+
+	const origin = (await headers()).get("origin");
+	const redirectUrl = await account.createOAuth2Token(
+		OAuthProvider.Github,
+		`${origin}/api/oauth`,
+		`${origin}/sigin-failed`
+	);
+
+	return redirect(redirectUrl);
+}
+
+export async function signUpWithGoogle() {
+	const { account } = await createAdminClient();
+
+	const origin = (await headers()).get("origin");
+    
+	const redirectUrl = await account.createOAuth2Token(
+		OAuthProvider.Google,
+		`${origin}/api/oauth`,
+		`${origin}/sigin-failed`
+	);
+
+	return redirect(redirectUrl);
+}
