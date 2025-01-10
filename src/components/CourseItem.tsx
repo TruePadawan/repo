063ff6583aa1a359593ba8b32fd3215d6ccc63d8
Hermes <hermeschigoziri@gmail.com>
2025-01-10 15:10:44 +0100

    add EditCourseDialog component and integrate with CourseItem for course editing

--- src/components/CourseItem.tsx
+++ src/components/CourseItem.tsx
@@ -1,15 +1,13 @@
+"use client";
 import { CourseItemAttributes } from "@/lib/types";
-import {
-	Button,
-	Menu,
-	MenuTarget,
-	MenuDropdown,
-	MenuItem,
-} from "@mantine/core";
+import { Button, Menu } from "@mantine/core";
 import { IconDotsVertical } from "@tabler/icons-react";
 import Link from "next/link";
+import EditCourseDialog from "./EditCourseDialog";
+import { useDisclosure } from "@mantine/hooks";
 
 export default function CourseItem(props: CourseItemAttributes) {
+	const [opened, { open, close }] = useDisclosure(false);
 	return (
 		<Button
 			color="dark"
@@ -17,17 +15,23 @@ export default function CourseItem(props: CourseItemAttributes) {
 			component="li"
 			rightSection={
 				<Menu>
-					<MenuTarget>
+					<Menu.Target>
 						<IconDotsVertical />
-					</MenuTarget>
-					<MenuDropdown>
-						<MenuItem>Edit Course Details</MenuItem>
-						<MenuItem>Copy Link to Clipboard</MenuItem>
-						<MenuItem>Delete Course</MenuItem>
-					</MenuDropdown>
+					</Menu.Target>
+					<Menu.Dropdown>
+						<Menu.Item onClick={open}>
+							Edit Course Details
+						</Menu.Item>
+						<Menu.Item>Copy Link to Clipboard</Menu.Item>
+						<Menu.Item>Delete Course</Menu.Item>
+					</Menu.Dropdown>
 				</Menu>
 			}>
 			<Link href="/">{props.code}</Link>
+			<EditCourseDialog
+				course={props}
+				dialogProps={{ open, opened, close }}
+			/>
 		</Button>
 	);
 }
---
+++ src/components/EditCourseDialog.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { updateCourse } from "@/app/actions/courseActions";
+import { CourseItemAttributes } from "@/lib/types";
+import { Button, Modal, Textarea, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { notifications } from "@mantine/notifications";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+interface EditCourseDialogProps {
+	dialogProps: {
+		opened: boolean;
+		close: () => void;
+		open: () => void;
+	};
+	course: CourseItemAttributes;
+}
+
+export default function EditCourseDialog(props: EditCourseDialogProps) {
+	const { course, dialogProps } = props;
+	const router = useRouter();
+	const [submitBtnIsDisabled, setSubmitBtnIsDisabled] = useState(false);
+	const form = useForm({
+		mode: "uncontrolled",
+		initialValues: { code: course.code, description: course.description },
+		validate: {
+			code: (value) =>
+				value.trim().length == 0 ? "Course code is required" : null,
+		},
+	});
+
+	async function handleSubmit(values: typeof form.values) {
+		setSubmitBtnIsDisabled(true);
+		try {
+			const response = await updateCourse({ ...values, $id: course.$id });
+			notifications.show({
+				title: "Course updated",
+				message: `Course ${response.code} updated successfully`,
+			});
+
+			router.refresh();
+			dialogProps.close();
+		} catch (error) {
+			notifications.show({
+				title: "Course not updated",
+				message:
+					error instanceof Error
+						? error.message
+						: "An error occurred while updating the course",
+			});
+		}
+		setSubmitBtnIsDisabled(false);
+	}
+
+	return (
+		<>
+			<Modal
+				opened={dialogProps.opened}
+				onClose={dialogProps.close}
+				title={`Edit course - ${course.code}`}
+				size="lg"
+				centered>
+				<form
+					className="flex flex-col gap-2"
+					onSubmit={form.onSubmit(handleSubmit)}>
+					<TextInput
+						variant="filled"
+						size="md"
+						radius="xs"
+						label="Course code"
+						placeholder="CSC102"
+						name="code"
+						key={form.key("code")}
+						{...form.getInputProps("code")}
+						required
+					/>
+					<Textarea
+						variant="filled"
+						size="md"
+						radius="xs"
+						label="Course description"
+						name="description"
+						key={form.key("description")}
+						{...form.getInputProps("description")}
+					/>
+					<Button
+						type="submit"
+						color="gray"
+						size="md"
+						disabled={submitBtnIsDisabled}>
+						Update
+					</Button>
+				</form>
+			</Modal>
+		</>
+	);
+}
