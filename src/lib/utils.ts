ca1c5a98214ce9b8229911f3a2a9ea21e0ec7952
Hermes <hermeschigoziri@gmail.com>
2025-01-11 07:09:29 +0100

    feat: add parseSlidesList utility function to handle slides list parsing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,17 @@
-import { RecommendedTextsAttributes } from "./types";
+import { RecommendedTextsAttributes, SlidesListAttributes } from "./types";
 
 export const DATABASE_ID = String(process.env.NEXT_PUBLIC_DATABASE_ID);
 export const COLLECTION_ID = String(process.env.NEXT_PUBLIC_COLLECTION_ID);
 
 export function parseRecommendedTexts(rawRecommendedTexts?: string) {
-    if (!rawRecommendedTexts) return [];
+	if (!rawRecommendedTexts) return [];
 	const recommendedTexts: RecommendedTextsAttributes =
 		JSON.parse(rawRecommendedTexts);
 	return Object.values(recommendedTexts);
+}
+
+export function parseSlidesList(rawSlidesList?: string) {
+	if (!rawSlidesList) return [];
+	const slidesList: SlidesListAttributes = JSON.parse(rawSlidesList);
+	return Object.values(slidesList);
 }
